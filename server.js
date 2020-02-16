cb6ef07333c416e942e9760e2e41eb0666e1737a
Mehdi Jarraya <Mehdi.jarraya@gmail.com>
2020-02-16 21:30:23 +0100

    trying to deploy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, 'build');
-const port = process.env.PORT || 9000;
+const publicPath = path.join(__dirname, 'build');;
+const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
